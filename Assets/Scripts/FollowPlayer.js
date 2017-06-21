3436486ed2c5eb212d57a087c31a585d949c1a37
Atemo678 <tomyahu@gmail.com>
2017-06-21 00:59:56 -0400

    Made Main mechanic work
    polished animations, added camera angle mechanic

---
+++ Assets/Scripts/FollowPlayer.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public var player : GameObject;
+
+public var temp : Vector3;
+
+function Start () {
+
+}
+
+function Update () {
+	
+	temp = player.transform.localPosition;
+	temp.z = transform.localPosition.z;
+	transform.localPosition = temp;
+	
+	
+	
+}
--- Assets/Scripts/PlayerManager.js
+++ Assets/Scripts/PlayerManager.js
@@ -2,6 +2,7 @@
 
 //Animator Controller
 private var anim : Animator;
+
 //RigidBody
 private var rb : Rigidbody;
 
@@ -12,6 +13,10 @@ public var jumpSpeed : int;
 //Variables para caminar
 public var walkSpeed: int;
 
+//Variables para Animacion
+public var temp : Vector3;
+
+public var rot : GameObject;
 
 //errores
 var errorRot : float;
@@ -21,6 +26,7 @@ function Start () {
 	rb = GetComponent.<Rigidbody>();
 	
 	rb.isKinematic = false;
+	temp = transform.localScale;
 }
 
 function Update () {
@@ -46,19 +52,41 @@ function Update () {
 	if (!cerca(0.0,transform.eulerAngles.x,errorRot))
 		transform.eulerAngles = Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z);
 	
-	if (!cerca(0.0,transform.eulerAngles.y,errorRot))
-		transform.eulerAngles = Vector3(transform.eulerAngles.x, 0, transform.eulerAngles.z);
-	
 	if (!cerca(0.0,transform.eulerAngles.z,errorRot))
 		transform.eulerAngles = Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
 }
 
 function walk() {
 
+	print(rot.transform.localRotation.y);
+	
+	if (cerca(0,rot.transform.eulerAngles.y,1))
+		walkS();
+	else if (cerca(180,rot.transform.eulerAngles.y,1))
+		walkN();
+	else if (cerca(90,rot.transform.eulerAngles.y,1))
+		walkE();
+	else if (cerca(270,rot.transform.eulerAngles.y,1))
+	{
+		walkW();
+	}
+	
+	
+}
+
+function walkS() {
+
 	if (Input.GetKey("d") && !Input.GetKey("a"))
 	{
 		if (anim.GetInteger("State") == 0)
 			anim.SetInteger("State",1);
+		
+		
+		if (transform.localScale.x < 0)
+		{
+			temp.x *= -1;
+			transform.localScale = temp;
+		}
 		
 		rb.velocity = Vector3(walkSpeed, rb.velocity.y, 0);
 	}
@@ -67,7 +95,109 @@ function walk() {
 		if (anim.GetInteger("State") == 0)
 			anim.SetInteger("State",1);
 		
+		if (transform.localScale.x > 0)
+		{
+			temp.x *= -1;
+			transform.localScale = temp;
+		}
+		
 		rb.velocity = Vector3(-walkSpeed, rb.velocity.y, 0);
+	}
+	
+}
+
+function walkN() {
+
+	if (Input.GetKey("d") && !Input.GetKey("a"))
+	{
+		if (anim.GetInteger("State") == 0)
+			anim.SetInteger("State",1);
+		
+		
+		if (transform.localScale.x < 0)
+		{
+			temp.x *= -1;
+			transform.localScale = temp;
+		}
+		
+		rb.velocity = Vector3(-walkSpeed, rb.velocity.y, 0);
+	}
+	else if (Input.GetKey("a") && !Input.GetKey("d"))
+	{
+		if (anim.GetInteger("State") == 0)
+			anim.SetInteger("State",1);
+		
+		if (transform.localScale.x > 0)
+		{
+			temp.x *= -1;
+			transform.localScale = temp;
+		}
+		
+		rb.velocity = Vector3(walkSpeed, rb.velocity.y, 0);
+	}
+	
+}
+
+function walkW() {
+
+	if (Input.GetKey("d") && !Input.GetKey("a"))
+	{
+		if (anim.GetInteger("State") == 0)
+			anim.SetInteger("State",1);
+		
+		
+		if (transform.localScale.x < 0)
+		{
+			temp.x *= -1;
+			transform.localScale = temp;
+		}
+		
+		rb.velocity = Vector3(0, rb.velocity.y, walkSpeed);
+	}
+	else if (Input.GetKey("a") && !Input.GetKey("d"))
+	{
+		if (anim.GetInteger("State") == 0)
+			anim.SetInteger("State",1);
+		
+		if (transform.localScale.x > 0)
+		{
+			temp.x *= -1;
+			transform.localScale = temp;
+		}
+		
+		rb.velocity = Vector3(0, rb.velocity.y, -walkSpeed);
+	}
+	
+}
+
+function walkE() {
+
+	if (Input.GetKey("d") && !Input.GetKey("a"))
+	{
+		if (anim.GetInteger("State") == 0)
+			anim.SetInteger("State",1);
+		
+		
+		if (transform.localScale.x < 0)
+		{
+			temp.x *= -1;
+			transform.localScale = temp;
+		}
+		
+		rb.velocity = Vector3(0, rb.velocity.y, -walkSpeed);
+	}
+	else if (Input.GetKey("a") && !Input.GetKey("d"))
+	{
+		if (anim.GetInteger("State") == 0)
+			anim.SetInteger("State",1);
+		
+		if (transform.localScale.x > 0)
+		{
+			temp.x *= -1;
+			transform.localScale = temp;
+		}
+		
+		rb.velocity = Vector3(0, rb.velocity.y, walkSpeed);
 	}
 	
 }
